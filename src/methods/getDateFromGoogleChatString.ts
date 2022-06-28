7b06529176467158846a04753b64f94e9e1003f4
JoshuaCoquia <joshua.coquia@hotmail.com>
2022-06-27 19:57:57 -0700

    fix date function

--- src/methods/getDateFromGoogleChatString.ts
+++ src/methods/getDateFromGoogleChatString.ts
@@ -1,33 +1,57 @@
 export async function getDateFromChatString(date: string): Promise<Date> {
     return new Promise((resolvePromise, rejectPromise) => {
         try {
-            if (date) {
+            if (typeof date === 'string') {
                 const YYYY = date.match(/[0-9][0-9][0-9][0-9]/)!.toString();
                 const DD = date.match(/[0-9][0-9]/)!.toString();
-                const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-                let MM: string | number = monthList.indexOf(
-                    date.match(/January|February|March|April|May|June|July|August|September|October|November|December/)!.toString()
-                ) + 1;
-                if (MM.toString().length == 1) MM = `0${MM}`;
-                let time;
+                const monthList = [
+                    'January',
+                    'February',
+                    'March',
+                    'April',
+                    'May',
+                    'June',
+                    'July',
+                    'August',
+                    'September',
+                    'October',
+                    'November',
+                    'December',
+                ];
+                let MM: number = monthList.indexOf(
+                    date
+                        .match(/January|February|March|April|May|June|July|August|September|October|November|December/)!
+                        .toString()
+                );
+                if (MM.toString().length == 1) MM = parseInt(`0${MM}`);
+                let time: string;
+                let hours: number;
+                let minutes: number;
+                let seconds: number;
                 // regex source: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s06.html
-                if (!date.match('PM')) {
-                    time = date.match(/(1[0-2]|0?[1-9]):([0-5]?[0-9])/)!.toString();
-                    if (time.length === 4) time = `0${time}`;
+                time = date.match(/(1[0-2]|0?[1-9]):([0-5]?[0-9]):([0-5]?[0-9])/)!.toString();
+                const hoursWithoutPM = parseInt(
+                    time
+                        .match(/^(1[0-2]|0?[1-9])/)!
+                        .toString()
+                        .replace(':', '')
+                );
+                if (date.match('PM')) {
+                    hours = hoursWithoutPM + 12;
                 } else {
-                    time = date.match(/1[0-2]|0?[1-9]:[0-5]?[0-9]/)!.toString();
-                    const hoursWithoutPM = date.match(/(1[0-2]|0?[1-9]):[0-5]?[0-9]/)![1].toString().replace(':', '');
-                    time = time.replace(/1[0-2]|0?[1-9]/, hoursWithoutPM).toString();
-                    if (time.length === 4) time = `0${time}`;
+                    hours = hoursWithoutPM;
                 }
-                resolvePromise(new Date(`${YYYY}-${MM}-${DD}T${time}Z`));
+                time = time.replace(/^(1[0-2]|0?[1-9]):/, '');
+                minutes = parseInt(time.match(/^([0-5]?[0-9])/)!.toString());
+                time = time.replace(/^([0-5]?[0-9]):/, '');
+                seconds = parseInt(time.toString()!);
+                const finalDate: Date = new Date(Date.UTC(parseInt(YYYY), MM, parseInt(DD), hours, minutes, seconds, 0));
+                resolvePromise(finalDate);
             } else {
                 throw new TypeError(`Expected date to be type of string!`);
             }
-
         } catch (error) {
             rejectPromise(error);
         }
-
-    })
+    });
 }
